ca97366be139c1a2c9f32cf4e63bb482473104c7
Warley Henrique <warley.henrique93@gmail.com>
2020-01-08 21:21:40 +0000

    Create directories MVC

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const portServer = 8081;
 
 const app = express();
 
 
+/**Connect MongoDB */
+mongoose.connect("mongodb+srv://warleyhenrique:warleyhenrique@cluster0-nf5ir.gcp.mongodb.net/agenda?retryWrites=true&w=majority",{
+    useNewUrlParser: true, useUnifiedTopology: true});
 
-app.listen(8081);
+
+app.listen(portServer);
